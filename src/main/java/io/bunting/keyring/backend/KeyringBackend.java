80c330defc6dd4ea61ef168f46ea32ac05d3f279
Jared Bunting <jared@bunting.io>
2015-03-08 13:41:05 -0500

    Removing priority method from KeyringBackend.

--- src/main/java/io/bunting/keyring/backend/KeyringBackend.java
+++ src/main/java/io/bunting/keyring/backend/KeyringBackend.java
@@ -6,21 +6,6 @@ package io.bunting.keyring.backend;
 public interface KeyringBackend
 {
 	/**
-	 * Each backend class must supply a priority, a number (float or integer)
-	 * indicating the priority of the backend relative to all other backends.
-	 * The priority need not be static -- it may (and should) vary based
-	 * attributes of the environment in which is runs (platform, available
-	 * packages, etc.).
-	 *
-	 * A higher number indicates a higher priority. The priority should throw
-	 * a RuntimeError with a message indicating the underlying cause if the
-	 * backend is not suitable for the current environment.
-	 * As a rule of thumb, a priority between zero but less than one is
-	 * suitable, but a priority of one or greater is recommended.
-	 */
-	int priority();
-
-	/**
 	 * Get password of the username for the service.
 	 */
 	char[] getPassword(final String service, final String username);
